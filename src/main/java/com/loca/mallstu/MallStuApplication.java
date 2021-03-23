15e14222ae2f6ed4fd691f30d1c97f771770a75e
wangheng <wangheng@edsz9.com>
2021-03-23 15:26:02 +0800

    Initial commit

---
+++ src/main/java/com/loca/mallstu/MallStuApplication.java
@@ -0,0 +1,13 @@
+package com.loca.mallstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallStuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loca/mallstu/MallStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loca.mallstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallStuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
